f876e2663c1f2e177e5781c5f319d35a942b3fe6
Adrian Eka <eekkaaadrian@gmail.com>
2024-04-14 13:47:10 +0700

    Update Create & Index Trip Page and remove unnecessary path for controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,59 +1,28 @@
 class TripsController < ApplicationController
-  before_action :set_trip, only: %i[ show edit update destroy ]
+  before_action :set_trip, only: %i[ show ]
 
-  # GET /trips or /trips.json
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @trips = Trip.all
   end
 
-  # GET /trips/1 or /trips/1.json
   def show
+    @trip = Trip.find(params[:id])
+    @add_ons = @trip.add_ons
   end
 
-  # GET /trips/new
   def new
     @trip = Trip.new
+    @trip.add_ons.build
   end
 
-  # GET /trips/1/edit
-  def edit
-  end
-
-  # POST /trips or /trips.json
   def create
     @trip = Trip.new(trip_params)
-
-    respond_to do |format|
-      if @trip.save
-        format.html { redirect_to trip_url(@trip), notice: "Trip was successfully created." }
-        format.json { render :show, status: :created, location: @trip }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /trips/1 or /trips/1.json
-  def update
-    respond_to do |format|
-      if @trip.update(trip_params)
-        format.html { redirect_to trip_url(@trip), notice: "Trip was successfully updated." }
-        format.json { render :show, status: :ok, location: @trip }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /trips/1 or /trips/1.json
-  def destroy
-    @trip.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to trips_url, notice: "Trip was successfully destroyed." }
-      format.json { head :no_content }
+    if @trip.save
+      redirect_to @trip, notice: 'Trip was successfully created.'
+    else
+      render :new
     end
   end
 
@@ -65,6 +34,6 @@ class TripsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def trip_params
-      params.require(:trip).permit(:name, :start_date, :end_date)
+      params.require(:trip).permit(:name, :start_date, :end_date, :description, :price)
     end
 end
