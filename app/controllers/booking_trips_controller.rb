817853806e083786ea78e4d97ba67da924be7039
Adrian Eka <eekkaaadrian@gmail.com>
2024-04-14 15:02:04 +0700

    Add backend validation for booking add on spot limits

--- app/controllers/booking_trips_controller.rb
+++ app/controllers/booking_trips_controller.rb
@@ -11,7 +11,7 @@ class BookingTripsController < ApplicationController
     @trip = Trip.find(params[:trip_id])
     @booking_trip = @trip.booking_trips.build(booking_trip_params)
     @booking_trip.user = current_user
-    if @booking_trip.save
+    if @booking_trip.save && booking_trip_valid_for_spots_limit?
       redirect_to @trip, notice: 'Trip booked successfully.'
     else
       render :new
@@ -22,5 +22,15 @@ class BookingTripsController < ApplicationController
 
   def booking_trip_params
     params.require(:booking_trip).permit(add_on_ids: [])
+  end
+
+  def booking_trip_valid_for_spots_limit?
+    @booking_trip.add_ons.each do |add_on|
+      unless add_on.bookable?
+        errors.add(:base, "#{add_on.name} is fully booked")
+        return false
+      end
+    end
+    true
   end
 end
